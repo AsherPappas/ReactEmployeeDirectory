bf14c95a778e15a1eab2c39915cb4cd63309ca1d
AsherPappas <73271785+AsherPappas@users.noreply.github.com>
2021-08-26 00:31:57 -0600

    building react app

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Test Industries, Inc.</h1>
+            <h2>Employee Directory</h2>
+        </div>
+
+    )
+}
+
+export default Header;
